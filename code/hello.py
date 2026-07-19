ffa707914b0cf40b256cbd3803f383e707900fb0
JF-code-master <2910231468@qq.com>
2026-07-19 15:48:15 +0800

    Initialize Python learning repository

---
+++ code/hello.py
@@ -0,0 +1,22 @@
+# Day01 Python基础
+
+print("Hello Python!")
+
+name = "JF"
+
+age = 20
+
+print(name)
+print(age)
+
+# Day02 python基础
+
+tmp=5/3
+print(tmp)
+
+l=5**3
+print(l)
+
+m=5.75*2-3
+print(m)
+
