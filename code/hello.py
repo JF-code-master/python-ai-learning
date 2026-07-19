6c9a58c5aec141aaa1541a6dcb0084cb8a3abe88
JF-code-master <2910231468@qq.com>
2026-07-19 16:05:37 +0800

    Update gitignore rules

--- code/hello.py
+++ code/hello.py
@@ -9,14 +9,5 @@ age = 20
 print(name)
 print(age)
 
-# Day02 python基础
 
-tmp=5/3
-print(tmp)
-
-l=5**3
-print(l)
-
-m=5.75*2-3
-print(m)
 
---
+++ code/数字.py
@@ -0,0 +1,10 @@
+# Day02 python基础
+
+tmp=5/3
+print(tmp)
+
+l=5**3
+print(l)
+
+m=5.75*2-3
+print(m)
